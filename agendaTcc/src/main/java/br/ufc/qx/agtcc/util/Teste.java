8463d5510823c83c05179959e5cc28ef6f1d9c1a
Anibal Oliveira <hanibal.ce80@gmail.com>
2017-12-03 18:31:16 -0300

    * Teste

--- agendaTcc/src/main/java/br/ufc/qx/agtcc/util/Teste.java
+++ agendaTcc/src/main/java/br/ufc/qx/agtcc/util/Teste.java
@@ -1,9 +1,12 @@
 package br.ufc.qx.agtcc.util;
 
+import java.util.Calendar;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		
+	
+		Calendar.getInstance().getTime();
 	}
 
 }
